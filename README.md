3decfc0dd8429409fd04222949cbd6ec5f1b1dd1
tangxiao <1076961087@qq.com>
2019-05-24 09:45:15 +0800

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-参考博客：  
+参考文章：  
 线程池：https://blog.csdn.net/u011677147/article/details/80271174  
-延迟队列：（1）https://www.cnblogs.com/DBGzxx/p/10090840.html   （2）https://www.jianshu.com/p/e40f55ecefda
+延迟队列：（1）https://www.cnblogs.com/DBGzxx/p/10090840.html   （2）https://www.jianshu.com/p/e40f55ecefda  
+redis订单缓存：https://tech.imdada.cn/2017/06/30/daojia-redis/
+
 
 延迟队列场景：未支付订单30分钟取消<br>
-ps: 所有订单进入消息队列设置30分钟过期时间，过期会进入死信队列，消费死信队列中的订单做一些业务处理（如未支付则取消订单）
+ps: 所有订单进入消息队列设置30分钟过期时间，过期会进入死信队列，消费死信队列中的订单做一些业务处理（如未支付则取消订单） <br>
+![死信队列](https://img2018.cnblogs.com/blog/1366148/201812/1366148-20181209125550537-1736839659.png)
+
--- src/main/java/com/tx/smalldemo/miaosha/service/OrderService.java
+++ src/main/java/com/tx/smalldemo/miaosha/service/OrderService.java
@@ -15,6 +15,14 @@ public class OrderService {
 
     @Autowired
     private OrderDAO orderDAO;
+    // 这里使用128个锁
+    private final static String[] LOCKS = new String[128];
+
+    static {
+        for (int i = 0; i < 128; i++) {
+            LOCKS[i] = "lock_" + i;
+        }
+    }
 
     /***
      * 下单：
@@ -24,19 +32,51 @@ public class OrderService {
      */
     public String createOrder01(Order order) {
 
-        //*****第一种，最简单的方法，容易造成超出库存、重复订单的情况
-
-        //判断库存，如果 number <库存 提示 库存不足
-        //创建订单
-        int i = orderDAO.createOrder(order);
-        if (i == 0) {
-            return "下单失败";
-        }
-        // 发送通知
-        // 各种操作
-        return "下单成功";
+        // 业务操作
+        // 写入数据库
+        // 写入redis
+        // 出现异常循环5次，仍然失败则进入到消息队列
+        return "";
     }
 
+    public void getOrderByUId(Integer uId) {
+        /**
+         //
+
+
+
+         1.查询缓存,存在订单则直接换回
+         if(orderRedisCache.isOrderListExist(uId){
+         return getOrderListFromCache(uId);
+         }
+         // 2.缓存中没有，先上锁,用户id的hashcode和锁数量与运算
+         int index = uId.hashCode() & (locks.length -1);
+
+         try{
+         orderRedisCache.lock(locks[index])
+         // 上锁后再次判断缓存是否存在
+         if(orderRedisCache.isOrderListExist(uId){
+         return getOrderListFromCache(uId);
+         }
+         // 查询数据库
+         list = orderDAO.getOrderListDb(uId);
+         // 如果数据库中无数据，就存入缓存中null,标识这个用户真的没有数据,等新订单入库，会删除此标识，放入订单数据
+         if (list == null && list.size() = 0){
+         cacheCloud.zAdd(orderRedisKey.getListKey(uId),0,null);
+         }else{
+         cacheCloud.zAdd(orderRedisKey.getListKey(uId),list);
+         }
+         return list;
+         }
+         finally{
+         orderRedisCache.unLock(locks[index]);
+         }
+
+
+         */
+
+
+    }
 
 
     @Data
